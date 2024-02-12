b33d9979b75dcfab892138f93d02aca4fd48efe6
Ali Feruz <147985313+AliFeruz@users.noreply.github.com>
2024-02-12 11:02:51 +0100

    edited eslint and Img for deployment

--- components/LeftSideBar.tsx
+++ components/LeftSideBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { signOut, useSession } from 'next-auth/react';
-import { UserCircleIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 
 const LeftSideBar = () => {
     const {data:session} = useSession();
@@ -25,7 +25,7 @@ const LeftSideBar = () => {
         bg-clip-text text-transparent uppercase p-1">Comme Le Roi</h1></Link>
         <div className='flex w-4/5 bg-blue-100 border border-cyan-500 
         my-2 drop-shadow-xl mx-1 gap-2 p-1 rounded-lg items-center'>
-             <img src={session?.user?.image || ''} alt="userimage" 
+             <Image src={session?.user?.image || ''} alt="userimage" 
              className="w-8 rounded-full h-8"/>
              <span className="px-2">{session?.user?.name}</span>
            </div>
